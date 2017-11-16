b524625181378e94fd5d2c52e29f9e1a0a784bac
Kevin Nowland <kevin.nowland@gmail.com>
2017-11-15 21:22:20 -0500

    fourfactors: initial implementation, not useful

--- fourfactors.py
+++ fourfactors.py
@@ -3,12 +3,13 @@ Here we examine using the four factors to predict point
 differential in games.
 """
 
-from sklearn import linear_model
+from sklearn import linear_model as lm
+from sklearn.metrics import r2_score
 import gamestats as gs
 import numpy as np
 import misc
 
-make_model(dates):
+def make_model(dates):
     """
     makes the linear regression model for the four factors between
     the given dates.
@@ -17,29 +18,39 @@ make_model(dates):
     for i in range(30):
         for j in range(30):
             if i != j:
-                team_combos.append([misc.ALL_TEAMS[i], misc.ALL_TEAMS[j])
+                team_combos.append([misc.ALL_TEAMS[i], misc.ALL_TEAMS[j]])
 
     gamefiles = gs.find_gamefiles(dates, team_combos)
 
     x = np.zeros((len(gamefiles), 8))
-    y = no.zeros(len(gamefiles))
+    y = np.zeros(len(gamefiles))
 
     row = 0
     for gamefile in gamefiles:
-        hometeam = gamefiles[-7:-4]
+        hometeam = gamefile[-7:-4]
+        print(gamefile)
         stats, opp_stats, raw_stats, raw_opp_stats, _ = \
-                gs.get_lineup_stats(hometeam, [gamefile], \
+                gs.get_lineup_stats(hometeam, [], [gamefile], \
                 return_raw = True, whole_team = True)
-        x[row][0] = stats['efg']
+        x[row][0] = stats['efg%']
         x[row][1] = stats['ftr']
-        x[row][2] = stats['tov']
-        x[row][3] = stats['orb']
-        x[row][4] = opp_stats['efg']
+        x[row][2] = stats['to%']
+        x[row][3] = stats['orb%']
+        x[row][4] = opp_stats['efg%']
         x[row][5] = opp_stats['ftr']
-        x[row][6] = opp_stats['tov']
-        x[row][7] = opp_stats['orb']
+        x[row][6] = opp_stats['to%']
+        x[row][7] = opp_stats['orb%']
         y[row] = raw_stats['pts'] - raw_opp_stats['pts']
         row += 1
+
+    linear = lm.LinearRegression(normalize = True)
+    linear.fit(x, y)
+
+    y_pred = linear.predict(x)
+
+    print(linear.coef_)
+    print(linear.intercept_)
+    print(r2_score(y, y_pred))
 
 
     return -1
